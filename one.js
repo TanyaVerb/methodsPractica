c0a56d165f0babd20769f1e9203a9bb170cbfde0
TanyaVerb <karra_t@mail.ru>
2024-05-08 01:36:07 +0300

    change function handlerCheckAnswer

--- one.js
+++ one.js
@@ -1,48 +1,73 @@
-const answers = ['1', '2', '3']
+const answers = ["1", "2", "3"];
 
 const cards = [
-    {
-        input: document.querySelector('.input1'),
-        btn:document.querySelector('.btn1'),
-    },
-    {
-        input: document.querySelector('.input2'),
-        btn:document.querySelector('.btn2'),
-    },
-    {
-        input: document.querySelector('.input3'),
-        btn:document.querySelector('.btn3'),
-    },
-]
+  {
+    input: document.querySelector(".input1"),
+    btn: document.querySelector(".btn1"),
+    angry: document.querySelector(".angry1"),
+    good: document.querySelector(".good1"),
+  },
+  {
+    input: document.querySelector(".input2"),
+    btn: document.querySelector(".btn2"),
+    angry: document.querySelector(".angry2"),
+    good: document.querySelector(".good2"),
+  },
+  {
+    input: document.querySelector(".input3"),
+    btn: document.querySelector(".btn3"),
+    angry: document.querySelector(".angry3"),
+    good: document.querySelector(".good3"),
+  },
+];
 
-const getAnswer =(index)=>{
-    return answers[index]
-}
+const getAnswer = (index) => {
+  return answers[index];
+};
 
-let count = 0
-const handlerCheckAnswer = function(index){
-console.log(cards);
-console.log(index);
-console.log(getAnswer(index));
+let count = 0;
+// const handlerCheckAnswer = function (index) {
+//   console.log(cards);
+//   console.log(index);
+//   console.log(getAnswer(index));
 
-if (this.input.value===getAnswer(index)){
-this.input.disabled = true
-this.btn.disabled = true
-count++
-if (count===3){
-    alert ('Вы ответили правильно')
-    window.location.href = 'htpps://google.com'
-}
+//   if (this.input.value === getAnswer(index)) {
+//     this.input.disabled = true;
+//     this.btn.disabled = true;
+//     count++;
+//     if (count === 3) {
+//       //   alert("Вы ответили правильно");
+//       this.good.style.display = "block";
 
-}else{
-    this.input.value = ''
-    alert('Ответ неверный, попробуйте угадать 1,2 или 3')
-}
-}
+//       //   window.location.href = "https://google.com";
+//     }
+//   } else {
+//     this.input.value = "";
+//     // alert("Ответ неверный, попробуйте угадать 1,2 или 3");
+//     this.angry.style.display = "block";
+//   }
+// };
+const handlerCheckAnswer = function (index) {
+  console.log(cards);
+  console.log(index);
+  console.log(getAnswer(index));
 
-cards.
-sort(()=> Math.random()-0.5)
-.forEach((card, index)=> {
-    card.btn.addEventListener('click',handlerCheckAnswer.bind(card, index) )
-})
+  if (this.input.value === getAnswer(index)) {
+    this.input.disabled = true;
+    this.btn.disabled = true;
 
+    this.good.style.display = "block";
+
+    //   window.location.href = "https://google.com";
+  } else {
+    this.input.value = "";
+    // alert("Ответ неверный, попробуйте угадать 1,2 или 3");
+    this.angry.style.display = "block";
+  }
+};
+
+cards
+  .sort(() => Math.random() - 0.5)
+  .forEach((card, index) => {
+    card.btn.addEventListener("click", handlerCheckAnswer.bind(card, index));
+  });
